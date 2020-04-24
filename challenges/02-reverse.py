b27a68b27ce8aea1d0886104f37bb26cdbab62a6
scumdestroy <35894688+scumdestroy@users.noreply.github.com>
2020-04-23 18:52:08 -0700

    Update 02-reverse.py

--- challenges/02-reverse.py
+++ challenges/02-reverse.py
@@ -6,3 +6,13 @@
 # several ways to reverse a string, and it's a good read!
 #
 # http://www.techbeamers.com/essential-python-tips-tricks-programmers/?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more#tip1
+
+print("Let's get REVERSED")
+print("Shout out to Issirocanob and E-vorg")
+whylol = input("Enter your next typed string: ")
+print("well, that wasn't even cool really...")
+print("here...")
+print(whylol[::-1])
+
+## practice that path of least hideous, most logical, simple to drop on your pop
+## serpentinian inversions komplete xo
